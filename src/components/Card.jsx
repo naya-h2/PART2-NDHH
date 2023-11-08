cbc5f7dc4414b7dfb11c10c445e51464b5388b81
nicolekim <nicolekim0115@gmail.com>
2023-11-08 20:46:42 +0900

    refactor: Card 컴포넌트 컨벤션 적용

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import propTypes from "prop-types";
-import Badge from "./Badge";
+import Badge from "@/components/commons/Badge";
 import defaultImg from "@/assets/default_profile.svg";
 import deleteIcon from "@/assets/trash_icon.svg";
 import plusIcon from "@/assets/plus_icon.svg";
-import { formatDate } from "../utils/formatDate";
-import { FONT12, FONT18, FONT20, FONT20B } from "../styles/FontStyles";
+import { formatDate } from "@/utils/formatDate";
+import { FONT12, FONT18, FONT20, FONT20B } from "@/styles/FontStyles";
 
 Card.propTypes = {
   type: propTypes.oneOf(["Normal", "Edit", "Plus"]),
@@ -47,10 +47,10 @@ export default Card;
 
 const Container = styled.div`
   width: 100%;
-  max-width: 384px;
-  min-width: 320px;
-  height: 280px;
-  padding: 28px 24px 24px;
+  max-width: 38.4rem;
+  min-width: 32rem;
+  height: 28rem;
+  padding: 2.8rem 2.4rem 2.4rem;
 
   position: relative;
 
@@ -58,64 +58,71 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 16px;
+  gap: 1.6rem;
 
-  border-radius: 16px;
+  border-radius: 1.6rem;
   background-color: var(--White);
-  box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
+  box-shadow: 0px 0.2rem 1.2rem 0px rgba(0, 0, 0, 0.08);
 
   @media (max-width: 1050px) {
-    max-width: 500px;
+    max-width: 50rem;
   }
 `;
-const ProfileImg = styled.img`
-  width: 56px;
-  height: 56px;
 
-  border-radius: 100px;
+const ProfileImg = styled.img`
+  width: 5.6rem;
+  height: 5.6rem;
+
+  border-radius: 10rem;
   border: 1px solid var(--Gray2);
   background: var(--white);
 `;
+
 const Profile = styled.div`
   width: 100%;
-  padding-bottom: 16px;
+  padding-bottom: 1.6rem;
 
   display: flex;
-  gap: 14px;
+  gap: 1.4rem;
 
-  border-bottom: 1px solid var(--Gray2);
+  border-bottom: 0.1rem solid var(--Gray2);
 `;
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 6px;
+  gap: 0.6rem;
   justify-content: center;
   align-items: flex-start;
 `;
+
 const Sender = styled.div`
   ${FONT20};
   color: var(--Black);
   line-height: 120%;
 `;
+
 const Bold = styled.span`
   ${FONT20B};
   line-height: 120%;
 `;
+
 const DeleteIcon = styled.img`
-  width: 40px;
-  height: 40px;
+  width: 4rem;
+  height: 4rem;
 
   position: absolute;
-  top: 28px;
-  right: 24px;
+  top: 2.8rem;
+  right: 2.4rem;
 
   &:hover {
     cursor: pointer;
   }
 `;
+
 const Content = styled.div`
   width: 100%;
-  height: 106px;
+  height: 10.6rem;
 
   overflow: hidden;
 
@@ -123,12 +130,12 @@ const Content = styled.div`
   text-overflow: ellipsis;
   white-space: nowrap;
 
-  /* Font/18 Regular */
   ${FONT18}
   font-family : ${({ $font }) => $font};
   line-height: 155.556%;
   letter-spacing: -0.018rem;
 `;
+
 const Date = styled.div`
   width: 100%;
 
@@ -137,9 +144,10 @@ const Date = styled.div`
   text-align: start;
   letter-spacing: -0.006rem;
 `;
+
 const PlusIcon = styled.img`
-  width: 56px;
-  height: 56px;
+  width: 5.6rem;
+  height: 5.6rem;
 
   &:hover {
     cursor: pointer;
