2a76c5bb7b60b8374cb033f68120331e042cdb95
티라노 <kde98892@gmail.com>
2023-11-08 20:06:13 +0900

    Refactor: Option, OptionProfile 컴포넌트 컨벤션 적용

--- src/components/commons/Option.jsx
+++ src/components/commons/Option.jsx
@@ -1,8 +1,8 @@
+import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
-import { FONT14 } from "../../styles/FontStyles";
-import { PropTypes } from "prop-types";
-import CHECKIMG from "../assets/check.svg";
-import { COLOR } from "../../styles/ColorStyles";
+import { COLOR } from "@/styles/ColorStyles";
+import { FONT14 } from "@/styles/FontStyles";
+import CHECKIMG from "@/assets/check.svg";
 
 Option.propTypes = {
   color: PropTypes.oneOf(["Orange", "Purple", "Blue", "Green"]),
@@ -53,19 +53,19 @@ const Container = styled.button`
     padding-bottom: 100%;
   }
 
+  border: 0.1rem solid rgba(0, 0, 0, 0.08);
+  border-radius: 1.6rem;
+
   display: flex;
   justify-content: center;
   align-items: center;
-
-  border: 0.1rem solid rgba(0, 0, 0, 0.08);
-  border-radius: 1.6rem;
 
   /* @media (max-width: 768px) {
     width: 15.4rem;
     height: 15.4rem;
   } */
 
-  ${FONT14};
+  ${FONT14}
   ${({ color }) => color};
   ${({ src, $check }) =>
     src && $check
@@ -76,11 +76,11 @@ const Container = styled.button`
     width: 4.4rem;
     height: 4.4rem;
 
+    border-radius: 10rem;
+
     display: flex;
     justify-content: center;
     align-items: center;
-
-    border-radius: 10rem;
 
     background-color: var(--Gray5);
   }
