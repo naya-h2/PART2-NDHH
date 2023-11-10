85ee40af4735ee6c9be71f171d49484fc5379255
티라노 <kde98892@gmail.com>
2023-11-11 03:21:24 +0900

    Refactor: 파일 삭제

--- src/api/getBackgroundImages.js
+++
@@ -1,9 +0,0 @@
-import actionResponse from "./api";
-
-const getBackgroundImages = async (type, method) => {
-  const data = await actionResponse(type, method);
-
-  return data;
-};
-
-export default getBackgroundImages;
