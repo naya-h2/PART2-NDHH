4b14364a8f08b285218d2aad44c83abd88362b4b
티라노 <kde98892@gmail.com>
2023-11-11 03:25:40 +0900

    test : 작업중 (delete 오류 수정, post 데이터 생성 함수 작성)

---
+++ src/api/postRecipient.js
@@ -0,0 +1,35 @@
+const { TEAM } = require("./config");
+
+export const CreateRecipient = (recipient, backgroundColor, backgroundImageURL) => {
+  return {
+    postData: {
+      team: TEAM,
+      name: recipient,
+      backgroundColor: backgroundColor,
+      backgroundImageURL: backgroundImageURL,
+    },
+  };
+};
+
+export const CreateMessage = (recipientId, sender, profileImageURL, relationship, content, font) => {
+  return {
+    postData: {
+      team: TEAM,
+      recipientId: recipientId,
+      sender: sender,
+      profileImageURL: profileImageURL,
+      relationship: relationship,
+      content: content,
+      font: font,
+    },
+  };
+};
+
+export const CreateEmoji = (emoji, type) => {
+  return {
+    postData: {
+      emoji: emoji,
+      type: type,
+    },
+  };
+};
