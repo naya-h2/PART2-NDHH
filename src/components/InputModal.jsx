99bb26db001982d78edfdf6b29e263001719d2b5
안희원 <hw7666@sogang.ac.kr>
2023-11-10 15:57:51 +0900

    ✨ Feat : Input 컴포넌트 비밀번호 에러 케이스 추가
    - 비밀번호를 틀렸을 때 에러 메시지가 되도록 Input 컴포넌트 수정

--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -3,24 +3,28 @@ import { FONT18 } from "@/styles/FontStyles";
 import styled from "styled-components";
 import Input from "./commons/Input";
 import Button from "./commons/Button";
+import { useState } from "react";
 
 function InputModal({ password, onClose }) {
+  const [pwError, setPwError] = useState(false);
   const navigate = useNavigate();
 
   const handlePasswordCheck = (event) => {
     const inputValue = event.target.children[1].children[0].value;
     event.preventDefault();
     if (inputValue == import.meta.env.VITE_EDIT_KEY || inputValue == password) {
+      setPwError(false);
       onClose();
       return navigate("/post/id/edit");
     }
+    setPwError(true);
   };
 
   return (
     <Container onSubmit={handlePasswordCheck}>
       <Text>🔐 비밀번호를 입력하세요.</Text>
       <InputWrapper>
-        <Input placeholder="●●●●" />
+        <Input placeholder="●●●●" pwError={pwError} />
       </InputWrapper>
       <Button width="100" height="l" type="primary">
         확인
