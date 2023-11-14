c7b578e3d9ce9d9a5bb62a1ad1b50c990932af73
nicolekim <nicolekim0115@gmail.com>
2023-11-14 21:42:08 +0900

    refactor: 스크롤바, 버튼 기능 수정 및 페이지 구조 변경

--- src/components/instances/FixedButton.jsx
+++ src/components/instances/FixedButton.jsx
@@ -14,7 +14,8 @@ function FixedButton({ children, link }) {
   return (
     <Container onClick={handleClick}>
       <Button type="primary" height="xl">
-        <ButtonText>{children}</ButtonText>
+        {/* <ButtonText>{children}</ButtonText> */}
+        {children}
       </Button>
     </Container>
   );
@@ -22,21 +23,27 @@ function FixedButton({ children, link }) {
 
 export default FixedButton;
 
-const ButtonText = styled.p`
-  width: 23.2rem;
-
-  ${FONT18B}
-
-  @media (max-width: ${DeviceSize.pc}) {
-    width: calc(100vw - 9.6rem);
-  }
-`;
-
 const Container = styled.div`
+  width: 28rem;
+
   position: fixed;
-  bottom: 4rem;
+  bottom: 7rem;
 
   @media (max-width: ${DeviceSize.pc}) {
-    bottom: 0.5rem;
+    bottom: 2.4rem;
   }
 `;
+
+// const ButtonText = styled.p`
+//   width: 23.2rem;
+
+//   ${FONT18B}
+
+//   @media (max-width: ${DeviceSize.pc}) {
+//     width: calc(100vw - 15rem);
+//   }
+
+//   @media (max-width: ${DeviceSize.mobile}) {
+//     width: calc(100vw - 9.2rem);
+//   }
+// `;
--- src/pages/list/Layout.jsx
+++ src/pages/list/Layout.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { DeviceSize } from "@/styles/DeviceSize";
 import ListPageCards from "@/components/instances/ListPageCards.jsx";
 import FixedButton from "@/components/instances/FixedButton.jsx";
-import { FONT20B, FONT24B } from "@/styles/FontStyles.js";
+// import { FONT20B, FONT24B } from "@/styles/FontStyles.js";
 import { sortHot, sortNew } from "@/utils/sort";
 import { Link } from "react-router-dom";
 import useGetData from "@/hooks/useGetData";
@@ -15,38 +15,56 @@ function Layout() {
     const HottestCards = sortHot([...Cards]);
 
     return (
-      <Container>
-        <div>
-          <P>인기 롤링 페이퍼 🔥</P>
-          <ListPageCards cards={HottestCards} />
-          <P $Mobile>최근에 만든 롤링 페이퍼 ⭐️️</P>
-          <ListPageCards cards={NewestCards} />
-        </div>
+      <>
+        <Container>
+          {/* <div> */}
+          {/* <P>인기 롤링 페이퍼 🔥</P> */}
+          <ListPageCards cards={HottestCards}>인기 롤링 페이퍼 🔥</ListPageCards>
+          {/* <P $Mobile>최근에 만든 롤링 페이퍼 ⭐️️</P> */}
+          <ListPageCards cards={NewestCards}>최근에 만든 롤링 페이퍼 ⭐️</ListPageCards>
+          {/* </div> */}
+        </Container>
         <FixedButton link="/post">나도 만들어보기</FixedButton>
-      </Container>
+      </>
     );
   }
 }
 
 export default Layout;
 
-const P = styled.p`
-  margin: 5rem 0 1.6rem 2rem;
-
-  ${FONT24B};
-
-  @media (max-width: ${DeviceSize.mobile}) {
-    ${FONT20B};
-    margin: ${(props) => (props.$Mobile ? "7.2rem 0 1.2rem 2rem" : "4rem 0 1.2rem 2rem")};
-  }
-`;
-
 const Container = styled.div`
-  width: 100%;
+  /* width: 100%; */
+  width: calc(100vw - 9.6rem);
+  max-width: 120rem;
+  margin: auto;
+
   margin-top: 0;
+  margin-bottom: 13.7rem;
+  /* margin-left: 2.4rem; */
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: ${DeviceSize.pc}) {
+    width: calc(100vw - 3.8rem);
+    margin-left: 2.4rem;
+  }
+
+  @media (max-width: ${DeviceSize.mobile}) {
+    width: calc(100vw - 3.8rem);
+    margin-left: 2rem;
+  }
 `;
+
+// const P = styled.p`
+//   margin: 5rem 0 1.6rem 2rem;
+
+//   ${FONT24B};
+
+//   @media (max-width: ${DeviceSize.mobile}) {
+//     ${FONT20B};
+//     margin: ${(props) => (props.$Mobile ? "7.2rem 0 1.2rem 2rem" : "4rem 0 1.2rem 2rem")};
+//   }
+// `;
