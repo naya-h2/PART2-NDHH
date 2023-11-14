8c7fe411d932fa354ca9d7ca07690b4aa8c6296b
티라노 <kde98892@gmail.com>
2023-11-14 15:16:05 +0900

    Refactor: post 페이지 헤더 복구

--- src/pages/post/PostPage.jsx
+++ src/pages/post/PostPage.jsx
@@ -2,13 +2,20 @@ import Header from "@/components/Header.jsx";
 import useGetWindowWidth from "@/hooks/useGetWindowWidth";
 import { DeviceSizeNum } from "@/styles/DeviceSize";
 import Layout from "./Layout";
+import useGetData from "@/hooks/useGetData";
+import { useParams } from "react-router";
 
 function PostPage({ page }) {
   const windowWidth = useGetWindowWidth();
 
+  const { id } = useParams();
+
+  const userData = useGetData("RECIPIENTS_ID", "GET", id);
+
   return (
     <>
       {windowWidth > DeviceSizeNum.mobile && <Header />}
+      <Header userData={userData} serviceType />
       <Layout path={page} />
     </>
   );
