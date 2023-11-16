3d62ac016cc66f6b39c215ea98e0bfcd295b02f6
nicolekim <nicolekim0115@gmail.com>
2023-11-16 14:02:11 +0900

    refactor: 추가 수정 조금..

--- src/components/instances/Skeleton.jsx
+++ src/components/instances/Skeleton.jsx
@@ -2,41 +2,48 @@ import React from "react";
 import ContentLoader from "react-content-loader";
 import { styled } from "styled-components";
 
-const Skeleton = (props) => {
+const Skeleton = () => {
   const cardWidth = 275;
   const cardHeight = 260;
-  const gap = 20;
+  const searchWidth = 700;
+  const searchHeight = 58;
+  const titleWidth = 200;
+  const titleHeight = 36;
+  const gapX = 20;
+  const gapY1 = 50;
+  const gapY2 = 16;
   const br = 20; // border radius
 
   const x1 = 0;
-  const x2 = x1 + cardWidth + gap;
-  const x3 = x2 + cardWidth + gap;
-  const x4 = x3 + cardWidth + gap;
+  const x2 = x1 + cardWidth + gapX;
+  const x3 = x2 + cardWidth + gapX;
+  const x4 = x3 + cardWidth + gapX;
 
-  const y1 = 0;
-  const y2 = y1 + 16 + 36;
-  const y3 = y2 + cardHeight + 50;
-  const y4 = y3 + 16 + 36;
+  const y1 = searchHeight + gapY1;
+  const y2 = y1 + gapY2 + titleHeight;
+  const y3 = y2 + cardHeight + gapY1;
+  const y4 = y3 + gapY2 + titleHeight;
 
   return (
     <Container>
-      <ContentLoader viewBox="0 0 1160 820" height="100%" width="100%" {...props}>
-        <rect x={x1} y={y1} rx="0" ry="0" width="200" height="36" />
+      <ContentLoader viewBox="0 0 1160 820" height="100%" width="100%" backgroundColor="#f5f5f5" foregroundColor="#dbdbdb">
+        {/* searchbar */}
+        <rect x={230} y={0} rx="15" ry="15" width={searchWidth} height={searchHeight} />
+
+        {/* cardList1 */}
+        <rect x={x1} y={y1} rx="0" ry="0" width={titleWidth} height={titleHeight} />
+
         <rect x={x1} y={y2} rx={br} ry={br} width={cardWidth} height={cardHeight} />
-
         <rect x={x2} y={y2} rx={br} ry={br} width={cardWidth} height={cardHeight} />
-
         <rect x={x3} y={y2} rx={br} ry={br} width={cardWidth} height={cardHeight} />
-
         <rect x={x4} y={y2} rx={br} ry={br} width={cardWidth} height={cardHeight} />
 
-        <rect x={x1} y={y3} rx="0" ry="0" width="200" height="36" />
+        {/* cardList2 */}
+        <rect x={x1} y={y3} rx="0" ry="0" width={titleWidth} height={titleHeight} />
+
         <rect x={x1} y={y4} rx={br} ry={br} width={cardWidth} height={cardHeight} />
-
         <rect x={x2} y={y4} rx={br} ry={br} width={cardWidth} height={cardHeight} />
-
         <rect x={x3} y={y4} rx={br} ry={br} width={cardWidth} height={cardHeight} />
-
         <rect x={x4} y={y4} rx={br} ry={br} width={cardWidth} height={cardHeight} />
       </ContentLoader>
     </Container>
