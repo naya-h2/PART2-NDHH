db8b6bbb97e6faea8c200e019233a87c1cdaa4f7
nicolekim <nicolekim0115@gmail.com>
2023-11-16 17:18:30 +0900

    refactor: 404 페이지 되돌리기....

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,8 @@
-import { FONT16B, FONT28B } from "@/styles/FontStyles";
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
+import logoImg from "@/assets/Logo.svg";
+import { Link } from "react-router-dom";
+import { FONT16B, FONT28B } from "@/styles/FontStyles";
 
 function NotFoundPage() {
   return (
@@ -12,18 +13,17 @@ function NotFoundPage() {
 
       <Container>
         <Wrapper>
-          <H2>Page Not Found</H2>
-          <H1>｡° ૮₍°´ᯅ`°₎ა °｡</H1>
+          <Link to="/">
+            <Logo src={logoImg} />
+          </Link>
           <Text>
             <P1>
               접근 권한이 없거나 존재하지 않는 페이지입니다!
               <br />
               This page does not exist.
             </P1>
+            <P2>｡° ૮₍°´ᯅ`°₎ა °｡</P2>
           </Text>
-          <Link to="/">
-            <HomeButton>Return Home</HomeButton>
-          </Link>
         </Wrapper>
       </Container>
     </>
@@ -36,63 +36,40 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
 
-  position: absolute;
-  background: linear-gradient(153deg, rgba(171, 87, 255, 1) 0%, rgba(220, 185, 255, 1) 50%, rgba(248, 240, 255, 1) 100%);
+  background-color: var(--purple1);
 `;
 
 const Wrapper = styled.div`
-  width: 80rem;
-  height: 50rem;
+  max-width: 60rem;
+  min-height: 18rem;
 
-  margin: 10% auto;
+  margin: auto;
+  padding-top: 25rem;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 3rem;
-
-  border-radius: 1.5rem;
-  box-shadow: 0 0 25px 14px rgba(0, 0, 0, 0.27);
-  background-color: var(--White);
+  gap: 5rem;
 `;
 
-const H2 = styled.p`
-  ${FONT28B}
-  color: var(--purple9)
-`;
-
-const H1 = styled.p`
-  font-size: 80px;
+const Logo = styled.img`
+  width: 15rem;
+  height: auto;
 `;
 
 const Text = styled.div`
   margin: 3rem 0;
 
-  text-align: center;
+  display: flex;
+  gap: 4rem;
 `;
 
 const P1 = styled.p`
   ${FONT16B}
   line-height: 200%;
 
-  color: var(--Gray5);
+  color: var(--purple9);
 `;
 
 const P2 = styled.p`
   ${FONT28B}
-`;
-
-const HomeButton = styled.p`
-  width: 15rem;
-  height: auto;
-
-  padding: 1rem;
-
-  border: 2px solid var(--purple7);
-  border-radius: 99rem;
-
-  text-align: center;
-  color: var(--purple7);
-  ${FONT16B};
 `;
